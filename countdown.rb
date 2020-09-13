0ef1d6a5c77b156a47d9d41dfb30f369b6ca09fe
Terrell Murrell <littlerell12@gmail.com>
2020-09-13 15:32:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,18 @@
 #write your code here
 
 def countdown(number)
-
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-
   "HAPPY NEW YEAR!"
+end
 
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
