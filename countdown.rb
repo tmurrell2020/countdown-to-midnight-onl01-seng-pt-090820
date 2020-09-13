2b4e27cf0dd00afb1bbbd1dd23fa84982a17b978
Terrell Murrell <littlerell12@gmail.com>
2020-09-13 15:19:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+number = 10
 
-def countdown
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECOND(S)"
+    number -= 1
+  end
+
+  puts "HAPPY NEW YEAR!"
+
 end
